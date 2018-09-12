07658820cc0efcd56a56a74a108e54cc4087e729
Connor Kutz <connorkutz@SEAS13887.local>
2018-09-12 13:13:39 -0400

    added volume slider to control music volume level

--- app/src/main/java/kutz/connor/testharness/TestActivity.java
+++ app/src/main/java/kutz/connor/testharness/TestActivity.java
@@ -1,7 +1,11 @@
 package kutz.connor.testharness;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.AudioManager;
+import android.widget.SeekBar;
+
 
 public class TestActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+
+        //creates and instance of AudioManager
+        final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        //gets the current volume and max level of music stream
+        int maximumLevel = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentLevel = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //initializes seek bar
+        SeekBar musicVolumeSeekBar = (SeekBar)findViewById(R.id.musicVolumeSeekBar);
+
+        //sets seek bar to current volume level
+        musicVolumeSeekBar.setMax(maximumLevel);
+        musicVolumeSeekBar.setProgress(currentLevel);
+
+        //creates active listener and applies to seekbar view
+        SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onStopTrackingTouch(SeekBar musicVolumeSeekBar) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar musicVolumeSeekBar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar musicVolumeSeekBar, int level, boolean fromUser)
+            {
+                audioManager.setStreamVolume(audioManager.STREAM_MUSIC, level, 0);
+            }
+        };
+        
+        musicVolumeSeekBar.setOnSeekBarChangeListener(onSeekBarChangeListener);
     }
+
+
 }
