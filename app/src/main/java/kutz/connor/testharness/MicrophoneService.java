7adf6d12b48053201fa196de3042f52cbea2733a
connorkutz <connorkutz@gwu.edu>
2018-09-30 17:28:01 -0400

    microphone service notification fixed

--- app/src/main/java/kutz/connor/testharness/MicrophoneService.java
+++ app/src/main/java/kutz/connor/testharness/MicrophoneService.java
@@ -1,14 +1,14 @@
 package kutz.connor.testharness;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.DrawableRes;
-import android.Manifest.permission;
 
 public class MicrophoneService extends Service{
 
@@ -32,7 +32,8 @@ public class MicrophoneService extends Service{
     @Override
     public int onStartCommand (Intent intent, int flags, int startId)
     {
-        Notification notification = new Notification.Builder(this, Notification.CATEGORY_SERVICE)
+        createNotificationChannel();
+        Notification notification = new Notification.Builder(this, getString(R.string.microphoneServiceNotificationChannelID))
                 .setSmallIcon(android.R.drawable.sym_def_app_icon)
                 .setContentTitle(getText(R.string.microphoneServiceNotificationTitle))
                 .setContentText(getText(R.string.microphoneServiceNotificationMessage))
@@ -50,5 +51,16 @@ public class MicrophoneService extends Service{
         public ServiceHandler(Looper looper){
             super(looper);
         }
+    }
+
+    private void createNotificationChannel(){
+        CharSequence name = getString(R.string.microphoneServiceNotificationChannelName);
+        String description = getString(R.string.microphoneServiceNotificationChannelDescription);
+        String id = getString(R.string.microphoneServiceNotificationChannelID);
+        int importance = NotificationManager.IMPORTANCE_MAX;
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
