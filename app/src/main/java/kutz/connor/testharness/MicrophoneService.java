06e51962ea4a4034cdf8a948737fd1d28efba33a
connorkutz <connorkutz@gwu.edu>
2018-09-30 16:44:58 -0400

    service permission granted, need to fix notification to start service

--- app/src/main/java/kutz/connor/testharness/MicrophoneService.java
+++ app/src/main/java/kutz/connor/testharness/MicrophoneService.java
@@ -1,11 +1,27 @@
 package kutz.connor.testharness;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.DrawableRes;
+import android.Manifest.permission;
 
 public class MicrophoneService extends Service{
+
+    private MediaRecorder mediaRecorder;
+    private static final int ONGOING_NOTIFICATION_ID = 1;
+
+    @Override
+    public void onCreate()
+    {
+        mediaRecorder = new MediaRecorder();
+    }
+
+
     @Override
     public IBinder onBind(Intent intent)
     {
@@ -16,13 +32,13 @@ public class MicrophoneService extends Service{
     @Override
     public int onStartCommand (Intent intent, int flags, int startId)
     {
+        Notification notification = new Notification.Builder(this, Notification.CATEGORY_SERVICE)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getText(R.string.microphoneServiceNotificationTitle))
+                .setContentText(getText(R.string.microphoneServiceNotificationMessage))
+                .build();
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
         return Service.START_STICKY;
-    }
-
-    @Override
-    public void onCreate()
-    {
-        super.onCreate();
     }
 
     @Override
--- app/src/main/java/kutz/connor/testharness/TestActivity.java
+++ app/src/main/java/kutz/connor/testharness/TestActivity.java
@@ -1,10 +1,14 @@
 package kutz.connor.testharness;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.AudioManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 
 public class TestActivity extends AppCompatActivity {
@@ -14,16 +18,17 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-
         //creates and instance of AudioManager
         final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        //initializes seek bar and buttons
+        SeekBar musicVolumeSeekBar = (SeekBar)findViewById(R.id.musicVolumeSeekBar);
+        Button startServiceButton = (Button)findViewById(R.id.startServiceButton);
+        Button endServiceButton = (Button)findViewById(R.id.endServiceButton);
 
         //gets the current volume and max level of music stream
         int maximumLevel = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int currentLevel = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-
-        //initializes seek bar
-        SeekBar musicVolumeSeekBar = (SeekBar)findViewById(R.id.musicVolumeSeekBar);
 
         //sets seek bar to current volume level
         musicVolumeSeekBar.setMax(maximumLevel);
@@ -45,9 +50,24 @@ public class TestActivity extends AppCompatActivity {
         };
 
         musicVolumeSeekBar.setOnSeekBarChangeListener(onSeekBarChangeListener);
+
+
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startServiceIntent = new Intent(v.getContext(), MicrophoneService.class);
+                Toast.makeText(getApplicationContext(), "start service", Toast.LENGTH_SHORT).show();
+                startForegroundService(startServiceIntent);
+
+            }
+        });
+        endServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent endServiceIntent = new Intent(v.getContext(), MicrophoneService.class);
+                Toast.makeText(getApplicationContext(), "end service", Toast.LENGTH_SHORT).show();
+                stopService(endServiceIntent);
+            }
+        });
     }
-
-
-
-
 }
